f6508450e3deb484b3be3d52269d5f7ce467583c
Medelouali <Mohammedelouali1999@gmail.com>
2021-07-09 09:41:37 -0700

    firstl commit of my portfolio

--- src/components/body/About/About.jsx
+++ src/components/body/About/About.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Intro from './Intro/Intro';
 import Passions from './Passions/Passions';
 import Studies from './Studies/Studies';
+import "./about.css";
 
 export default function About() {
     return (
-        <>
+        <div className="about">
             <Intro/>
             <Studies/>
             <Passions/>
-        </>
+        </div>
     )
 }
