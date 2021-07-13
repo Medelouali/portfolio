4671cbca155f5cd3e1586a9bccca5fa03a6f28a7
Medelouali <Mohammedelouali1999@gmail.com>
2021-07-13 05:15:13 -0700

    linking the projects

--- src/components/body/About/Intro/Intro.jsx
+++ src/components/body/About/Intro/Intro.jsx
@@ -23,7 +23,7 @@ export default function Intro() {
                 </div>
                 <div className="intro">
                     <p className="intro-text">
-                        <span className="first-letter">L</span>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        <span className="first-letter">M</span>y expertise is in the area of responsive user-centric web design. With every line of code, I strive to make the web a beautiful place!
                     </p>
                     <div className="deco"><AcUnitIcon/><AcUnitIcon/><AcUnitIcon/></div>
                 </div>
--- src/vars/passions/passion.js
+++ src/vars/passions/passion.js
@@ -18,19 +18,41 @@ const passions=[
         logo: problem,
         passion: "Problem Solving",
         text: `
-            I got into web developpement because I had in mind some projects to build, in other 
-            words, I had problems. 
+            The thing I love about math is its precision to solve problems. The reason I started 
+            programming in first place is that I faced some problems and I needed to solve them, 
+            but math wasn't enough so I started coding and you know what you get when you combine 
+            math with coding?! you get the key to solve any problem. For instance, you can't talk 
+            about AI without bringing up math. In short, this is how I loot at it, my math skills 
+            let me write the code needed to solve specific puzzles in a logical way, some people 
+            deny this idea saying that in order to get into web dev math is not a requirement, 
+            I agree but once you are in and you want to create something relevant at some point 
+            you'll need math. 
         `
     },
     {
         logo: music,
         passion: "Music",
-        text: ``
+        text: `
+            Life without music is boring to me, when I code I listen to music, when I take a break 
+            I listen to music, when I am walking the streets I listen to music... Generally I let 
+            the music on for like 3 hours a day. I listen to almost all kinds of music from Rap to 
+            Pop.
+        `
     },
     {
         logo: hacking,
         passion: "Hacking",
-        text: ``
+        text: `
+            For a guy who has started programming with C, at some point he/she will realize that 
+            his/her C knowledge is what is needed to get into the world of hacking especially if 
+            you are good at algorithms and data structures picking up some scripting language like 
+            Python. I remember the first time I made a buffer-over-flow exploit it felt amazing, 
+            or when I cracked the wifi password of my neighbors I felt good having access to the 
+            internet for free. I always found myself barricaded over the internet and hacking was 
+            the way to get out of the box and be aware that there's no privacy. One of the things 
+            I always put up in mind when working on my web apps is to write secure code and avoid 
+            the crap.  
+        `
     },
 ]
 
