2b27c3e5d76b520daf768e6d5e867c0fb997bd23
Medelouali <Mohammedelouali1999@gmail.com>
2021-07-15 02:45:46 -0700

    zomming in feature in the image component

--- src/components/body/Projects/Project/Image.jsx
+++ src/components/body/Projects/Project/Image.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./image.css";
+// import { motion } from "framer-motion";
 
 export default function Image({img}) {
+    const [open, setOpen] = useState(false);
+    const handleOpen=()=>{
+        setOpen(true);
+    };
+
+    const handleClose=()=>{
+        setOpen(false);
+    };
     return (
-        <div className="img">
-            <img src={img} alt="" className="center-img" />
-        </div>
+        <>
+            <div
+                onMouseEnter={ handleOpen }
+                onMouseLeave={ handleClose }
+                className="img">
+                <img src={img} alt="" className={"center-img"+ (open ? " on": "")} />
+            </div>
+        </>
     )
 }
--- src/components/body/Projects/Project/Project.jsx
+++ src/components/body/Projects/Project/Project.jsx
@@ -5,7 +5,9 @@ import "./project.css";
 export default function Project({project}) {
     return (
         <div className="project">
-            <h4 className="project-title">{project.title}</h4>
+            <h4 className="project-title">
+                <a href={project.link} className="link-to">{project.title}</a>
+            </h4>
             <div className="images">{project.images.map(img=>(<Image img={img} />))}</div>
             <div className="project-description">{project.description}</div>
             <div className="project-link">
